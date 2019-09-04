505d0ceb45c3b5d07f2a654ff070cf5d149dac50
Dmitry Golovko <dzmitry.halauko@itechart-group.com>
2019-09-04 14:47:46 +0300

    design and navigation

--- app/src/main/java/com/itechart/turvotest/screens/TickersFragment.kt
+++ app/src/main/java/com/itechart/turvotest/screens/TickersFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.itechart.turvotest.R
 
 class TickersFragment : Fragment() {
@@ -14,5 +15,12 @@ class TickersFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? = inflater.inflate(R.layout.fragment_tickers, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<View>(R.id.next).setOnClickListener {
+            findNavController().navigate(R.id.portfolioFragment)
+        }
+    }
 
 }
