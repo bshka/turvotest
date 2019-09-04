5121808562a29cf02940018f398f35fc8a8200b3
Dmitry Golovko <dzmitry.halauko@itechart-group.com>
2019-09-04 12:25:54 +0300

    initial

---
+++ app/src/main/java/com/itechart/turvotest/screens/DetailsFragment.kt
@@ -0,0 +1,18 @@
+package com.itechart.turvotest.screens
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.itechart.turvotest.R
+
+class DetailsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_details, container, false)
+
+}
